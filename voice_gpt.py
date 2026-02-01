ebcbc4324cd6a29c4b08cc81d66d64220935da6d
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2026-02-01 18:27:38 +0530

    Update voice_gpt.py

--- voice_gpt.py
+++ voice_gpt.py
@@ -113,7 +113,7 @@ def translate_to_english(text):
     )
 
     data = response.json()
-    st.wtite(data)
+    st.write(data)
     return data[0]["translations"][0]["text"]
 
 
@@ -137,4 +137,5 @@ if uploaded_audio:
 
         st.subheader("🌍 English Translation")
         st.success(english)
+
 
